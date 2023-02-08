b37e9b082d9df506026b2fc8da8095ee1e49768e
gopichand <gopichand.g0693@gmail.com>
2023-02-08 13:52:58 +0530

    finished method notations

--- src/main/scala/lectures/part2oop/MethodNotations.scala
+++ src/main/scala/lectures/part2oop/MethodNotations.scala
@@ -4,8 +4,12 @@ object MethodNotations extends  App {
 
   class Person(val name: String, favouriteMovie: String) {
     def likes(movie:String):Boolean = movie == favouriteMovie
-
+    def +(person: Person) = s"this.name is hanging out with ${person.name}"
     def hangOutWith(person: Person):String = s"${this.name} is hanging  out with ${person.name}"
+    def isAlive:Boolean = true
+    def apply():String = s"Hi, my name is $name and I like $favouriteMovie"
+
+    def apply(n:Int):String = s"Hi my name is mary wath my fav movie $favouriteMovie $n times."
   }
 
   val mary = new Person("mary", "Inception")
@@ -14,6 +18,7 @@ object MethodNotations extends  App {
   println(mary likes "Inception") // infix notation, works only with single method
 
   // above infix notation is an example of syntactic sugar
+  // purpose of  syntactic sugar is to make Scala more natural language look alike
 
   // "operators" in scala
 
@@ -26,4 +31,17 @@ object MethodNotations extends  App {
   // prefix notation
 
   val x = -3
+
+  println(mary.apply())
+  // or
+  println(mary()) // both are same  or equivalent becasue it breaks barrier between FP and OOP
+  // i.e objects can be used to call as functions with apply method
+
+  /*
+  *  1. overload the +operator
+  *
+
+  *
+  * */
+
 }
