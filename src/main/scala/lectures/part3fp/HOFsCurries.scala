9656f892b0fa1a60486aef613cd980f1d6dc15cc
gopichand <gopichand.g0693@gmail.com>
2023-03-01 13:06:12 +0530

    removed hofs

--- src/main/scala/lectures/part3fp/HOFsCurries.scala
+++ src/main/scala/lectures/part3fp/HOFsCurries.scala
@@ -2,7 +2,7 @@ package lectures.part3fp
 
 object HOFsCurries extends App {
 
-  val superFunction : (Int, (String, (Int => Boolean)) => Int ) => (Int => Int) = ???
+  val superFunction : (Int, (String, (Int => Boolean)) => Int ) => (Int => Int) = null
 
   // such functions which take functions ans params and return the func as result are HOF
 
@@ -17,14 +17,56 @@ object HOFsCurries extends App {
     else  ntimes(f, n-1, f(x))
   }
 
-  val plusOne = (x:Int) => x+1
+  val plusOne : Int => Int  = (x:Int) => x+1
   println(ntimes(plusOne, 10, 1))
+
   def ntimesBetter(f: Int => Int, n: Int):(Int => Int) =
     if (n<= 0) (x:Int) => x
     else (x:Int) => ntimesBetter(f, n-1)(f(x))
 
-    println(ntimesBetter(plusOne, 10))
+  val plus10 = ntimesBetter(plusOne, 10)
+
+  println(ntimesBetter(plusOne, 10)(1))
+
+  // curried functions
+
+  //println(superAdder(10)(3))
+
+  // functions with multiple parameter lists
+
+  def curriedFormatter(c:String)(x:Double) : String = c.format(x)
+
+  val standardFormat : (Double => String) = curriedFormatter("%4.1f")
+
+  val preciseFormat : (Double => String) = curriedFormatter("%10.8f")
+
+  println(standardFormat(Math.PI))
+  println(preciseFormat(Math.PI))
+
+  /*
+  1. Expand MyList
+     - foreach method A => Unit
+     [1,2,3].foreach(x => println(x))
+    
+     - sort function ((A,A) => Int) => MyList
+     [1,2,3].sort((x,y) => y-x) => [3,2,1]
+    
+     - zipWith (list, (A,A) => B) => MyList[B]
+     - [1,2,3].zipWith([4,5,6], x*y) => [1*4, 2*5, 3*6] = [4,10,18]
+    
+     - fold(start)(function) => a value
+     [1,2,3].fold(0)(x+y) = 6
+     0+1 + 2 + 3
+  
+  2. toCurry(f: (Int, Int) => Int) => (Int => Int => Int)
+     fromCurry(f: (Int => Int => Int)) => (Int, Int) => Int
+  
+  3. compose(f,g) => x => f(g(x))
+     andThen(f,g) => x => g(f(x))
+  
+  
 
 
+  */
 
 }
